446f21d86733225758dc6b3c40cc33df8a2fcdfe
User111-ops <rahmahmida23@gmail.com>
2025-06-10 13:43:53 +0000

    Mise à jour du fichier haystack_pipeline.py

--- haystack_pipeline.py
+++ haystack_pipeline.py
@@ -1,19 +1,15 @@
 from haystack import Pipeline
+from haystack.nodes import SentenceTransformersTextEmbedder, DenseRetriever, OpenAITextEmbedder
 from haystack.document_stores import InMemoryDocumentStore
-from haystack.nodes import EmbeddingRetriever
-from haystack.nodes import DensePassageRetriever
 from haystack.utils import Document
 
 def create_pipeline(documents):
     document_store = InMemoryDocumentStore()
     document_store.write_documents(documents)
-
     query_embedder = OpenAITextEmbedder()
-    retriever = InMemoryEmbeddingRetriever(document_store=document_store)
-
+    retriever = DenseRetriever(document_store=document_store, embedding_model=query_embedder)
     pipeline = Pipeline()
-    pipeline.add_component("query_embedder", query_embedder)
-    pipeline.add_component("retriever", retriever)
-    pipeline.connect("query_embedder.embedding", "retriever.query_embedding")
-
+    pipeline.add_node(query_embedder, name="query_embedder", inputs=["query"])
+    pipeline.add_node(retriever, name="retriever", inputs=["query_embedder"])
+    
     return pipeline
